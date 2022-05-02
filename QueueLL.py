7765792c5d8e20e9cdc529c922b935e58cbf03f5
michaelUsh3R <98755535+michaelUsh3R@users.noreply.github.com>
2022-05-01 21:50:26 -0400

    Update QueueLL.py

--- QueueLL.py
+++ QueueLL.py
@@ -1,7 +1,7 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 3: Queue-LL
 #
-# NAME:         FIXME
+# NAME:         Michael Usher
 # ASSIGNMENT:   Project 5: Implementing ADTs
 
 from Node import Node
@@ -14,21 +14,30 @@ class QueueLL(object):
             for item in list:
                 self.enq(item)
 
-    def get_front(self):
-        # FIXME
-        return
+    def get_front(self): # returns data at the front of the queue
+        if self.front is not None:
+            return self.front.get_data()
+        return None
 
-    def get_tail(self):
-        # FIXME
-        return
+    def get_tail(self): # returns data at the tail of the queue
+        if self.tail is not None:
+            return self.tail.get_data()
+        return None
 
-    def deq(self):
-        # FIXME
-        return
+    def deq(self): # returns data at the front and removes the front node and updates front
+        if self.is_empty(): return None
+        front_node = self.front
+        self.front = self.front.get_next()
+        return front_node.get_data()
 
-    def enq(self, data=None):
-        # FIXME
-        return
+    def enq(self, data=None): # adds item to the back of the queue
+        new_node = Node(data)
+        if self.is_empty():
+            self.front = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def print(self):
         n = self.front
@@ -37,28 +46,24 @@ class QueueLL(object):
             n = n.get_next()
         print("NULL")
 
-    def is_empty(self):
-        # FIXME
-        return
+    def is_empty(self): # returns true if queue is empty
+        return self.front is None
 
-    def clear(self):
-        # FIXME
-        return
-
+    def clear(self): # clears all items in the queue
+        self.front = None
+        self.tail = None
 
 def main():
     s = QueueLL()
-    s.print()
-    print("Is empty?", s.is_empty())
-    for i in range(1, 7):
+    le = []
+    la = []
+    for i in range(20, 201, 10):
         s.enq(i)
-    s.print()
-    print("Peek:", s.peek())
-    print("Deq: ", s.deq())
-    s.print()
-    print("Is empty?", s.is_empty())
+        le.append(i)
+    while not s.is_empty():
+        la.append(s.deq())
+    print(le == la)
 
 # Don't run main on import
 if __name__ == "__main__":
     main()
-
