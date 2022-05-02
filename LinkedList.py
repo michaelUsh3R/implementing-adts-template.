7fcf367efcdfdc630dfa371cb907bc89ab6e92f5
michaelUsh3R <98755535+michaelUsh3R@users.noreply.github.com>
2022-05-01 21:49:15 -0400

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,7 +1,7 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 1: Linked List
 #
-# NAME:         FIXME
+# NAME:         Michael Usher
 # ASSIGNMENT:   Project 5: Implementing ADTs
 
 from Node import Node
@@ -14,20 +14,40 @@ class LinkedList(object):
                 self.add(item)
 
     def get_head(self):
-        # FIXME
+        if self.head is not None:  # check whether the head is none
+            return self.head.get_data()
+        else:
+            return None
+
+    def add(self, data): # make next node as head, and move head to point to node
+        node = Node(data)
+        node.next = self.head
+        self.head = node
         return
 
-    def add(self, data):
-        # FIXME
-        return
+    def search(self, data): # make current node the head, traverse till last node, if node with data found then return true
+        curr = self.head  # make curr node to head
 
-    def search(self, data):
-        # FIXME
-        return
+        while curr != None:
+            if curr.data == data:
+                return True
+            curr = curr.next
+        return False
 
-    def delete(self, data):
-        # FIXME
-        return
+    def delete(self, data): # deletes the selected node
+        curr = self.head
+        if curr != None and curr.data == data:
+            self.head = curr.next
+            curr = None
+            return data
+        while curr != None:
+            if curr.data == data:
+                prev.next = curr.next
+                curr = None
+                return data
+            prev = curr
+            curr = curr.next
+        return None
 
     def print(self):
         n = self.head
@@ -36,28 +56,33 @@ class LinkedList(object):
             n = n.get_next()
         print("NULL")
 
-    def is_empty(self):
-        # FIXME
-        return
+    def is_empty(self): # checks if empty by seeing if the head is none
+        if self.head is not None:
+            return False
+        else:
+            return True
 
-    def clear(self):
-        # FIXME
-        return
+    def clear(self): # deletes the list
+        temp = self.head
+        if temp is None:
+            print("\n Not possible to delete empty list")
+        while temp:
+            self.head = temp.get_next()
+            temp = None
+            temp = self.head
 
 def main():
-    l = list(range(1, 5))
+    l = []
     l.reverse()
     ll = LinkedList(l)
-    ll.print()
-    print("Search 4: ", ll.search(4))
-    print("Search 5: ", ll.search(5))
-    print("Delete 5: ", ll.delete(5))
-    print("Delete 4: ", ll.delete(4))
-    ll.print()
-    print("Delete 1: ", ll.delete(1))
-    ll.print()
+    le = []
+    la = []
+    for i in range(20, 201, 10):
+        ll.add(i)
+        le.append(i)
+    for i in range(20, 201, 10):
+        la.append(ll.delete(i))
+    print("Search 4: ",le==la)
 
-# Don't run main on import
 if __name__ == "__main__":
     main()
-
