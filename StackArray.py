a9c24d19269e54d729ea262c1e4d04cc74c9e0f8
michaelUsh3R <98755535+michaelUsh3R@users.noreply.github.com>
2022-05-09 23:45:47 -0400

    Update StackArray.py

--- StackArray.py
+++ StackArray.py
@@ -9,42 +9,50 @@ class StackArray(object):
         self.array = [0 for i in range(size)]
         self.top = -1
 
-    def peek(self):         # return array top as peek
+    def peek(self):  # return array top as peek
         return self.array[self.top]
 
-    def pop(self): # decreases top item and returns it
-        if (self.top == -1):
+    def pop(self):  # decreases top item and returns it
+        if (self.is_empty()):
             return None
         item = self.array[self.top]
         self.top = self.top - 1
         return item
 
     def push(self, data=None): # push when array isn't full
-        if (self.top < len(self.array) - 1):
-            self.top = self.top + 1;
-            self.array[self.top] = data;
-        return
+        if (self.is_full()):
+            return None
+        self.top = self.top + 1
+        self.array[self.top] = data
+        newArr = [0 for i in range(self.size() + 1)]
+        for i in range(0, self.size() - 1):
+            newArr[i] = self.array[i]
+
+        newArr[self.size() - 1] = data
+        self.array = newArr
+
+        return None
 
     def print(self):
         for i in range(self.top, -1, -1):
             print(self.array[i], "=>", end=" ")
         print("NULL")
 
-    def is_empty(self): # returns -1 if it is empty
+    def is_empty(self):  # returns -1 if it is empty
         return (self.top == -1)
 
-    def is_full(self): # returns -1 if it is full
+    def is_full(self):  # returns -1 if it is full
         return self.top == len(self.array) - 1
 
-    def clear(self): # delete all elements in array
+    def clear(self):  # delete all elements in array
         for i in range(0, self.top):
             self.array[self.top] == None
             self.top = self.top - 1
         self.top = -1
         return
 
-    def size(self):
-        return self.top + 1
+    def size(self): # gets the size
+        return self.top + 11
 
 def main():
     s = StackArray(5)
@@ -60,5 +68,3 @@ def main():
 # Don't run main on import
 if __name__ == "__main__":
     main()
-
-
